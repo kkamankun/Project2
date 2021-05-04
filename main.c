87879e3503f6ea1870ae83b5da20a552ef86b4f4
developerTae <pts322@naver.com>
2021-05-03 19:03:56 -0700

    filestat 함수 작성

--- main.c
+++ main.c
@@ -29,12 +29,12 @@ int main(){
 
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1(){
-    
+    stat("text1", &stat1);
 }
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(){
-    
+    stat("text2", &stat2);
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
